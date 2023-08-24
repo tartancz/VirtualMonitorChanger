38cdba744f11c9152dac4dbfcb6e47627f95a865
ruzicka <tartanec@gmail.com>
2023-08-24 10:29:00 +0200

    added middle mouse function in main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from changer import Changer, Buttons
 from time import sleep
 
 if __name__ == '__main__':
-    Changer(Buttons.RIGHT, 3, 5).start()
+    Changer(Buttons.RIGHT, 3, 0.5).start()
+    Changer(Buttons.MIDDLE, 2, 1).start()
     while True:
         sleep(3600)
